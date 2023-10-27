7ca9ae7eded6b62e83cb8cadb12575d92316047b
AshikRahmanDev <ashikrahman.dev@gmail.com>
2023-10-27 23:41:15 +0600

    remove address

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,27 +1,42 @@
 import React from 'react'
 
 const Banner = () => {
+
+    const handleSendMessage=(e)=>{
+        e.preventDefault()
+        const form = e.target;
+        const name = form.name.value;
+        const email= form.email.value;
+        const number=form.number.value;
+        const message=form.message.value;
+        console.log({
+            name,
+            email,
+            number,
+            message
+        })
+    }
   return (
     <div className='banner min-h-[90vh] flex items-center overflow-hidden'>
         <div className='md:w-[90%] xl:w-[1200px] mx-auto md:flex justify-between'>
             <div data-aos="fade-right" data-aos-duration="3000" className='w-[90%] md:w-[50%] mx-auto text-white my-10 md:m-0'>
                 <h4>Software Development</h4>
                 <h1 className='text-[40px] md:text-[64px] font-bold'>Bartholomew Development presents Overtime Athletic Management.</h1>
-                <p>Become a Tester with "Sign up for Discounted Beta Testing".</p>
+                <p>Say Goodbye to Fragmented Athletic Management. Try our beta and experience a unified platform that streamlines registration, scheduling, and more. Designed for Athletic Directors, by those who understand the game.</p>
                 <a href="mailto:joseph@bartholomewdevelopment.com"><button className='py-3 mt-6 bg-white text-primary rounded-lg px-6 shadow-lg text-[20px]'>Get A Quote</button></a>
             </div>
             <div data-aos="fade-left" data-aos-duration="3000" className='md:w-[50%] flex justify-center my-10 md:m-0'>
-                <form className='w-[90%] md:w-[60%] bg-white p-5 rounded-2xl'>
+                <div className='w-[90%] md:w-[60%] bg-white p-5 rounded-2xl'>
                     <h1 className='text-2xl font-bold'>Sign up for Discounted Beta Testing</h1>
-                    <p className='text-gray'>Let us know about your plan and strategy and, get a free quote.</p>
-                    <div>
-                        <input className='w-full my-4 py-2 border border-gray/40 px-3' type="text" placeholder='Full Name' />
-                        <input className='w-full my-4 py-2 border border-gray/40 px-3' type="text" placeholder='Email' />
-                        <input className='w-full my-4 py-2 border border-gray/40 px-3' type="number" placeholder='Phone' />
-                        <input className='w-full my-4 py-2 border border-gray/40 px-3' type="text" placeholder='Comments or Questions?' />
-                        <button className='py-2 bg-primary text-white border-primary border hover:text-primary hover:bg-white rounded-sm px-6 shadow-lg text-[16px]'>Send Message</button>
-                    </div>
-                </form>
+                    <p className='text-gray text-sm'>Elevate your athletic department's performance and streamline operations with Overtime Athletic Management. Sign up for our beta test today and secure a special discounted rate!</p>
+                    <form onSubmit={handleSendMessage}>
+                        <input required name='name' className='w-full my-4 py-2 border border-gray/40 px-3' type="text" placeholder='Full Name' />
+                        <input required name='email' className='w-full my-4 py-2 border border-gray/40 px-3' type="email" placeholder='Email' />
+                        <input required name='number' className='w-full my-4 py-2 border border-gray/40 px-3' type="number" placeholder='Phone' />
+                        <input required name='message' className='w-full my-4 py-2 border border-gray/40 px-3' type="text" placeholder='Comments or Questions?' />
+                        <button type='submit' className='py-2 bg-primary text-white border-primary border hover:text-primary hover:bg-white rounded-sm px-6 shadow-lg text-[16px]'>Send Message</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -4,6 +4,7 @@ import {AiOutlineTwitter} from "react-icons/ai"
 import {BiLogoLinkedin} from "react-icons/bi"
 import {BsFillTelephoneFill} from "react-icons/bs"
 import {MdEmail} from "react-icons/md"
+import {MdLocationPin} from "react-icons/md"
 
 export const Footer = () => {
   return (
@@ -12,26 +13,26 @@ export const Footer = () => {
             <div className='w-[90%] xl:w-[1200px] mx-auto flex flex-col justify-center items-center'>
                 {/* social icons */}
                 <div className='flex'>
-                    <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' href="">
+                    <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' target='_blank' href="https://www.facebook.com/profile.php?id=100092204199645">
                         <FaFacebookF className='text-primary text-2xl'/>
                     </a>
-                    <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' href="">
+                    <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' target='_blank' href="">
                         <AiOutlineTwitter className='text-primary text-2xl'/>
                     </a>
-                    <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' href="">
+                    <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' target='_blank' href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.linkedin.com%2Fcompany%2Fartifconnect%2F%3Ffbclid%3DIwAR0AhfiOi_rdWoCkFg1xPvZkl0k7z0xROysqoTX9RPGlFyxlIlOn8BrxukI&h=AT3Wp0WJt6tsLWLiG9k4i0Zboy4Zg7-m4YHlW4hcjwFeOYw98g2NuB-uH4PHiJFqQLsA-I6BwLqy3D9tt0EgdqqcoOtoYrYLBSZIgre-JyBPlTg3H1ZsGBDR-aWpAKp_IoLdTA">
                         <BiLogoLinkedin className='text-primary text-2xl'/>
                     </a>
                 </div>
                 {/* location */}
-                <div className='my-5 flex flex-col items-center pt-3'>
-                    <p className='flex items-center'><BsFillTelephoneFill className='mr-2 text-primary'/> <span>+880 8080 8080</span></p>
+                <div className='mt-5 flex flex-col items-center pt-3'>
+                    <p className='flex items-center'><BsFillTelephoneFill className='mr-2 text-primary'/> <span>801-380-6649</span></p>
                     <p className='flex items-center'><MdEmail className='mr-2 text-primary'/> <a href='mailto:joseph@bartholomewdevelopment.com'>joseph@bartholomewdevelopment.com</a></p>
                 </div>
             </div>
         </div>
         {/* mini footer */}
         <div className='py-3 text-center'>
-            <p className='px-2'>© 2023 BARTHOLOMEW All Rights Reserved. Desinged & Developed By BARTHOLOMEW</p>
+            <p className='px-2'>© 2023 BARTHOLOMEW DEVELOPMENT All Rights Reserved. Designed & Developed By Bartholomew Development LLC</p>
         </div>
     </div>
   )
