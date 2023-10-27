275c70ff8a6b8c11d71fd1e78bc1073f1a37c571
AshikRahmanDev <ashikrahman.dev@gmail.com>
2023-10-27 14:54:46 +0600

    footer animation added

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import {FaFacebookF} from "react-icons/fa"
 import {AiOutlineTwitter} from "react-icons/ai"
 import {BiLogoLinkedin} from "react-icons/bi"
+import {BsFillTelephoneFill} from "react-icons/bs"
+import {MdEmail} from "react-icons/md"
 
 export const Footer = () => {
   return (
-    <div className='h-[200px] bg-madBlack text-white flex justify-between items-center'>
+    <div data-aos="fade-up" className='min-h-[200px] bg-madBlack text-white flex justify-between items-center py-5 mt-6'>
         <div className='w-[90%] xl:w-[1200px] mx-auto flex flex-col justify-center items-center'>
             {/* social icons */}
             <div className='flex'>
-                <a className='w-[55px] h-[55px] bg-white rounded-full flex justify-center items-center mx-3' href="">
-                    <FaFacebookF className='text-primary text-3xl'/>
+                <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' href="">
+                    <FaFacebookF className='text-primary text-2xl'/>
                 </a>
-                <a className='w-[55px] h-[55px] bg-white rounded-full flex justify-center items-center mx-3' href="">
-                    <AiOutlineTwitter className='text-primary text-3xl'/>
+                <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' href="">
+                    <AiOutlineTwitter className='text-primary text-2xl'/>
                 </a>
-                <a className='w-[55px] h-[55px] bg-white rounded-full flex justify-center items-center mx-3' href="">
-                    <BiLogoLinkedin className='text-primary text-3xl'/>
+                <a className='w-[45px] h-[45px] bg-white rounded-full flex justify-center items-center mx-3' href="">
+                    <BiLogoLinkedin className='text-primary text-2xl'/>
                 </a>
             </div>
             {/* location */}
-            <div>
-                <p>amn tamn</p>
+            <div className='my-5 flex flex-col items-center pt-3'>
+                <p className='flex items-center'><BsFillTelephoneFill className='mr-2 text-primary'/> <span>+880 8080 8080</span></p>
+                <p className='flex items-center'><MdEmail className='mr-2 text-primary'/> <span>joseph@bartholomewdevelopment.com</span></p>
             </div>
         </div>
     </div>
