055b36e4861b78c369238be8cc33591fde4aac9f
faysalewucse <faysal.ewucse@gmail.com>
2023-11-17 07:18:38 +0600

    deployment changed to bartho dev

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Banner from '../../components/Banner'
-import { Services } from '../../components/Services'
-import { Footer } from '../shared/Footer'
-import { Toaster } from 'react-hot-toast'
-import { Portfolio } from '../../components/Portfolio'
+import Banner from "../../components/Banner";
+import { Services } from "../../components/Services";
+import { Footer } from "../shared/Footer";
+import { Toaster } from "react-hot-toast";
+import { Portfolio } from "../../components/Portfolio";
 
 export const Home = () => {
   return (
     <div>
-      <Banner/>
-      <Portfolio/>
-      <Services/>
-      <Footer/>
+      <Banner />
+      <Portfolio />
+      <Services />
+      <Footer />
       <Toaster />
     </div>
-  )
-}
+  );
+};
